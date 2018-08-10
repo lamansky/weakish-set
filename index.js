fa3e4604e33641869307a720496c45a1256b6aaa
Fr. John Lamansky <lamansky@users.noreply.github.com>
2018-08-10 12:49:57 -0500

    Support custom Set/WeakSet classes

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const weakable = require('weakable')
 const _set = Symbol('set')
 const _strong = Symbol('strong')
 const _weak = Symbol('weak')
+const _WeakSet = Symbol('WeakSet')
 
 module.exports = class WeakishSet {
-  constructor (items = []) {
-    this[_strong] = new Set()
-    this[_weak] = new WeakSet()
+  constructor (items = [], {StrongSet: Strong = Set, WeakSet: Weak = WeakSet} = {}) {
+    this[_strong] = new Strong()
+    this[_weak] = new Weak()
+    this[_WeakSet] = Weak
     for (const item of items) this.add(item)
   }
 
@@ -18,7 +20,7 @@ module.exports = class WeakishSet {
   has (item) { return this[_set](item).has(item) }
   add (item) { return this[_set](item).add(item) }
   delete (item) { return this[_set](item).delete(item) }
-  clear () { this[_strong].clear(); this[_weak] = new WeakSet() }
+  clear () { this[_strong].clear(); this[_weak] = new this[_WeakSet]() }
 
   entries (...args) { return this[_strong].entries(...args) }
   forEach (...args) { return this[_strong].forEach(...args) }
--- readme.md
+++ readme.md
@@ -10,7 +10,35 @@ Requires [Node.js](https://nodejs.org/) 6.0.0 or above.
 npm i weakish-map
 ```
 
-## Usage
+## API
+
+The module exposes a single class.
+
+### Constructor
+
+The constructor supports the following arguments:
+
+1. Optional: `items` (iterable): Initial key-value pairs for the Set.
+2. Optional: Object argument:
+    * `StrongSet` (class): Set this if you have a custom Set class you want to use for storing non-objects. Defaults to the built-in `Set`.
+    * `WeakSet` (class): Set this if you have a custom WeakSet class you want to use for storing objects. Defaults to the built-in `WeakSet`.
+
+### Methods
+
+Instances of this class have the following methods, which behave just like the corresponding methods on `Set` and `WeakSet`:
+
+* `has()`
+* `add()`
+* `delete()`
+* `clear()`
+
+Instances also have methods which only work on non-objects:
+
+* `entries()`
+* `forEach()`
+* `values()`
+
+## Example
 
 ```javascript
 // Before
